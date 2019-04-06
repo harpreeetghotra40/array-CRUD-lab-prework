7a0bb5ac1b08c62627c05af993193fc1414613a6
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-06 15:54:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  return array.index(index_number)
+  num =  array.index(index_number)
+  return num
 end
 
 def retrieve_first_element_from_array(array)
